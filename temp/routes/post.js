30196ecfe0c8c0f090b322c25c9ef9722cd78fe9
unknown <seowj819@gmail.com>
2020-09-30 16:12:05 +0900

    complete

--- temp/routes/post.js
+++ temp/routes/post.js
@@ -15,5 +15,28 @@ router.post('/', async (req, res, next) => {
     }
 });
 
+router.post('/update', async (req, res, next) => {
+    try{
+        await Post.update({
+            content: req.body.temp}, {where: {id: req.body.postId}}
+        );
+        res.redirect('/');
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try{
+        await Post.destroy({ 
+            where: { id: req.params.id, userId: req.user.id }
+        });
+        res.send('OK');
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
